9eec671f17d716b4e1143e75412a3720e2c2297f
m1sk9 <me@m1sk9.dev>
2024-09-27 23:02:54 +0900

    feat: Add Discord API login logic

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,40 @@
-fn main() {
-    println!("Hello, world!");
+mod handler;
+
+use serenity::prelude::GatewayIntents;
+use tracing_subscriber::{EnvFilter, FmtSubscriber};
+
+#[derive(serde::Deserialize)]
+pub struct FromisEnv {
+    pub discord_api_token: String,
+}
+
+pub fn from_env() -> &'static FromisEnv {
+    static ENV: std::sync::OnceLock<FromisEnv> = std::sync::OnceLock::new();
+    ENV.get_or_init(|| envy::from_env().expect("Failed to parse environment variables"))
+}
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let filter =
+        EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("fromis=debug"));
+    let subscriber = FmtSubscriber::builder().with_env_filter(filter).finish();
+    tracing::subscriber::set_global_default(subscriber)
+        .expect("Failed to set tracing_subscriber as global default.");
+
+    if let Err(why) = dotenvy::dotenv() {
+        Err(anyhow::anyhow!("Failed to load .env file: {}", why))?;
+    };
+
+    let envs = from_env();
+    let intents = GatewayIntents::MESSAGE_CONTENT | GatewayIntents::GUILD_MEMBERS;
+    let mut client = serenity::Client::builder(&envs.discord_api_token, intents)
+        .event_handler(handler::FromisHandler)
+        .intents(intents)
+        .await?;
+
+    if let Err(why) = client.start().await {
+        Err(anyhow::anyhow!("Failed to start the client: {}", why))?;
+    };
+
+    Ok(())
 }
