9ea7e8b785893852f31e45bc13829eb98a6ef8db
Sho Sakuma <me@m1sk9.dev>
2024-09-30 11:42:06 +0900

    feat: Add preview logic (#3)
    * deps: Add more dependencies
    
    * feat: Add preview logic
    
    * fix: Relace `GUILD_MEMBERS` intent to `GUILD_MESSAGE`

--- src/handler.rs
+++ src/handler.rs
@@ -1,10 +1,29 @@
-use serenity::all::{ActivityData, Context, Ready};
+use crate::github::GitHubPreview;
+use serenity::all::{ActivityData, Context, Message, Ready};
 use serenity::client::EventHandler;
 
 pub struct FromisHandler;
 
 #[serenity::async_trait]
 impl EventHandler for FromisHandler {
+    async fn message(&self, ctx: Context, message: Message) {
+        if message.author.bot || !GitHubPreview::is_exist(&message.content) {
+            return;
+        }
+
+        let preview = match crate::preview::get_preview(&message.content).await {
+            Ok(preview) => preview,
+            Err(e) => {
+                tracing::error!("Failed to get preview: {}", e);
+                return;
+            }
+        };
+
+        if let Err(why) = message.reply(&ctx.http, preview).await {
+            tracing::error!("Failed to send message: {}", why);
+        }
+    }
+
     async fn ready(&self, ctx: Context, client: Ready) {
         tracing::info!(
             "Connected as {}, id: {}",
---
+++ src/preview.rs
@@ -0,0 +1,39 @@
+use crate::github::{GitHubPreview, GitHubPreviewError};
+use std::fmt::Write;
+use tracing::log::debug;
+
+pub async fn get_preview(message: &str) -> Result<String, GitHubPreviewError> {
+    let permalink = GitHubPreview::find_from_str(message)?;
+    let code = permalink.get_code().await?;
+
+    debug!("GitHubPreview::get_preview: {:?}", permalink);
+
+    let mut is_backquote_replaced = false;
+    is_backquote_replaced |= code.contains("```");
+    let code = code.replace("```", "'''");
+
+    let mut msg = String::new();
+    let mut buf = String::new();
+
+    // -# is a Markdown syntax for small text
+    let _ = writeln!(
+        buf,
+        "-# {}/{}@{}: {}",
+        permalink.owner, permalink.repo, permalink.branch, permalink.path
+    );
+    let _ = writeln!(buf, "```{}", permalink.ext);
+    let _ = writeln!(buf, "{}", code);
+    let _ = writeln!(buf, "```");
+
+    msg.push_str(&buf);
+    buf.clear();
+
+    if is_backquote_replaced {
+        msg.insert_str(
+            0,
+            "-# Backquotes were included, so they were replaced with single quotes\n",
+        );
+    };
+
+    Ok(msg)
+}
